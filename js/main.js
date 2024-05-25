42b91e7f08b87f7ebcbfc73945c1cb3f4baf716b
aleksandrgolovichov <aleksandgolovicev@gmail.com>
2024-05-25 22:06:56 +0300

    update 100%

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let day = document.getElementById("day");
+let hours = document.getElementById("hours")
+let min = document.getElementById("min");
+let sec = document.getElementById("sec");
+
+console.log(day,hours,min,sec);
+let curentDate = new Date().getDate();
+console.log(curentDate)
+let birthday = new Date('26 may 2024 08:00:00')
+console.log(birthday)
+function timer(){
+
+    let todayDate = Date.now()
+    console.log(todayDate)
+
+    let gap = birthday - todayDate
+    console.log(gap)
+
+    let d = Math.floor(gap /1000 / 60 / 60 / 24);
+    let h = Math.floor((gap /1000 / 60 / 60 ) % 24);
+    let m = Math.floor((gap /1000/ 60 ) % 60);
+    let s = Math.floor((gap /1000) % 60);
+    console.log(d,h,m,s)
+     day.innerHTML = d;
+     hours.innerHTML = h;
+     min.innerHTML = m;
+     sec.innerHTML = s;
+    
+}setInterval(timer, 1000);
+timer();
